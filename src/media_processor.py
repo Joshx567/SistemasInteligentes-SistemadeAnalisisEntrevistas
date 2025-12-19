994a71c15fb0d1b15ef27225f1177bc628283c72
unknown <huascar.rivero@ucb.edu.bo>
2025-12-19 14:48:01 -0400

    agregar cache para imagenes o frames similares

--- src/media_processor.py
+++ src/media_processor.py
@@ -2,7 +2,7 @@ import os
 import cv2
 from moviepy import VideoFileClip
 
-def extract_media(video_path, output_base, video_name):
+def extract_media(video_path, output_base, video_name, frame_step=None):
     # Crear carpetas separadas por video
     frames_dir = os.path.join(output_base, "processed_frames\dia4", video_name)
     audio_dir = os.path.join(output_base, "processed_audio\dia4", video_name)
@@ -14,9 +14,13 @@ def extract_media(video_path, output_base, video_name):
     audio_path = os.path.join(audio_dir, "audio.wav")
     video_clip.audio.write_audiofile(audio_path, logger=None)
 
-    # Extraer frames (1 frame por segundo)
     cap = cv2.VideoCapture(video_path)
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Extraer frames (1 frame por segundo si no se especifica)
+    if frame_step is None:
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+    else:
+        fps = frame_step
     
     count = 0
     saved_count = 0
